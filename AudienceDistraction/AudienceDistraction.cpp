270b5dc6df170d1a6be7ca43c878c58110c4793b
Dominik <domkaukinen@gmail.com>
2014-11-01 10:31:34 -0400

    removed old cpp code

--- AudienceDistraction/AudienceDistraction.cpp
+++ AudienceDistraction/AudienceDistraction.cpp
@@ -16,8 +16,8 @@ using namespace cv;
 void detectAndDisplay(Mat frame);
 
 /** Global variables */
-String face_cascade_name = "c:/opencv/data/haarcascades/haarcascade_frontalface_alt.xml";
-String eyes_cascade_name = "c:/opencv/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml";
+String face_cascade_name = "haarcascade_frontalface_alt.xml";
+String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
 CascadeClassifier face_cascade;
 CascadeClassifier eyes_cascade;
 string window_name = "Capture - Face detection";
